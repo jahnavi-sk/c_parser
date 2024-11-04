393241ac8de65b86d6a62f8932fd7acaf1ffae00
jahnavi-sk <jahnavi31blr@gmail.com>
2024-11-04 15:36:08 +0530

    student teacher imagieee

---
+++ frontend/src/app/teacher/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function BackgroundBeamsWithCollisionDemo() {
+    return (
+      <div>
+        Girllll I'm a teacher !!!!
+      </div>
+    );
+  }
+  
